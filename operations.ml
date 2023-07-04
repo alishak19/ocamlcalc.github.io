255874c463a7faa92be249517575584489329009
Alisha Kumar <93096342+alishak19@users.noreply.github.com>
2023-07-04 13:46:46 -0500

    Update operations.ml

--- operations.ml
+++ operations.ml
@@ -1,38 +1,38 @@
 type exp = 
   | VAR of string
   | INT of int
-  | ADD of exp * exp
-  | SUB of exp * exp
-  | MUL of exp * exp
-  | DIV of exp * exp
   | SIGMA of exp * exp * exp * exp
+  | ADD of exp * exp
+  | SUBT of exp * exp
+  | MULT of exp * exp
+  | DIV of exp * exp
 
 module SMap = Map.Make (String)
 
-let rec aux_sigma var b env e =
-  let rec aux env acc =
-    let v = SMap.find var env in
+let rec aux_sigma var b enviro e =
+  let rec aux enviro acc =
+    let v = SMap.find var enviro in
     if v > b then acc
     else 
-      let res = calculator env e in
-      aux (SMap.add var (v + 1) env) (acc + res)
-  in aux env 0
+      let res = calculator enviro e in
+      aux (SMap.add var (v + 1) enviro) (acc + res)
+  in aux enviro 0
 
-and calculator env = function
+and calculator enviro = function
   | VAR x -> SMap.find x env
   | INT x -> x
-  | ADD (e1, e2) -> calculator env e1 + calculator env e2
-  | SUB (e1, e2) -> calculator env e1 - calculator env e2
-  | MUL (e1, e2) -> calculator env e1 * calculator env e2
-  | DIV (e1, e2) -> calculator env e1 / calculator env e2
   | SIGMA (var, e1, e2, e3) -> 
     match var with
     | VAR x ->
-      let init = calculator env e1 in 
-      let bound = calculator env e2 in
-      aux_sigma x bound (SMap.add x init env) e3
+      let initial = calculator enviro e1 in 
+      let bound = calculator enviro e2 in
+      aux_sigma x bound (SMap.add x initial enviro) e3
     | _ -> assert false
+  | ADD (e1, e2) -> calculator enviro e1 + calculator enviro e2
+  | SUBT (e1, e2) -> calculator enviro e1 - calculator enviro e2
+  | MULT (e1, e2) -> calculator enviro e1 * calculator enviro e2
+  | DIV (e1, e2) -> calculator enviro e1 / calculator enviro e2
 
-let e1 = SIGMA (VAR "X", INT 1, INT 10, SUB(MUL(VAR "X", VAR "X"), INT 1))
+let e1 = SIGMA (VAR "X", INT 1, INT 10, SUBT(MULT(VAR "X", VAR "X"), INT 1))
 
 let () = Printf.printf "%d\n" (calculator SMap.empty e1)
